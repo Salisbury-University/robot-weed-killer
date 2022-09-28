7250c8ed7201927d6091f91f40ebe49264298298
Garrett <95944392+qiugai@users.noreply.github.com>
2022-09-28 15:36:34 -0400

    created python control script, fixed arduino read

---
+++ BTcontrol/control.py
@@ -0,0 +1,17 @@
+import keyboard,serial, time
+ser=serial.Serial('com5',9600)
+while True:
+    if keyboard.is_pressed('w'):
+        ser.write(b'f')
+    elif keyboard.is_pressed("s"):
+        ser.write(b'b')
+    elif keyboard.is_pressed("d"):
+        ser.write(b'r')
+    elif keyboard.is_pressed("a"):
+        ser.write(b'l')
+    elif keyboard.is_pressed(" "):
+        ser.write(b'+')
+    else:
+        ser.write(b's')
+        ser.write(b'-')
+    time.sleep(.5)

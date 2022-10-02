c49de1d081b87033f6688dd9a5318aac55edb9ea
Garrett <95944392+qiugai@users.noreply.github.com>
2022-10-02 14:12:35 -0400

    added comments

--- BTcontrol/control.py
+++ BTcontrol/control.py
@@ -1,16 +1,25 @@
-import keyboard,serial, time
+#keyboard for reading user input, serial for sending bytes to arduino
+import keyboard,serial
+#create our serial named ser
 ser=serial.Serial('com5',9600)
+#infinite loop to read user input
 while True:
     if keyboard.is_pressed('w'):
+        #sends forward signal
         ser.write(b'f')
     elif keyboard.is_pressed("s"):
+        #sends reverse signal
         ser.write(b'b')
     elif keyboard.is_pressed("d"):
+        #sends turn right signal
         ser.write(b'r')
     elif keyboard.is_pressed("a"):
+        #sends turn left signal
         ser.write(b'l')
     elif keyboard.is_pressed(" "):
+        #sends laser fire signal
         ser.write(b'+')
     else:
+        #sends stop brake and disable laser signal
         ser.write(b's')
         ser.write(b'-')

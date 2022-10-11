93008f094a4fcdb2b3879b1ffc7ebf885b8d2394
Zach Moore <zmoore@Zachs-MacBook-Pro.local>
2022-10-11 15:11:50 -0400

    Completely restructured git file struture for frontend

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import ReactNipple from 'react-nipple';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return(
+    <View>
+      <Text>GOD PLEASE COMPILE OR SOMETHING</Text>
+    </View>
+  );
+};
+
+export default App;

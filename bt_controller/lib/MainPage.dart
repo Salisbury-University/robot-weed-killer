ebb46d787e081a57d93c8a00b5e551aac982b12c
Zach Moore <50252234+ZM009@users.noreply.github.com>
2022-11-17 15:41:34 -0500

    Main Page update; need to finish

---
+++ bt_controller/lib/MainPage.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+import './ChatPage.dart';
+import './DiscoveryPage.dart';
+import './SelectBondedDevicePage.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPage createState() => new _MainPage();
+}
+
+class _MainPage extends State<MainPage> {
+  BluetoothState _bluetoothState = BluetoothState.UNKNOWN;
+
+  String _address = "...";
+  String _name = "...";
+
+  Timer? _discoverableTimeoutTimer;
+  int _discoverableTimeoutSecondsLeft = 0;
+
+  bool _autoAcceptPairingRequests = false;
+}

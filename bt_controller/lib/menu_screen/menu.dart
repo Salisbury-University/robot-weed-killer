32166a043c201459d393b894d1fc9d43da2520a3
evHitch <107266441+evHitch@users.noreply.github.com>
2023-02-09 19:00:02 -0500

    fixed Menu layout & added components

--- bt_controller/lib/menu_screen/menu.dart
+++ bt_controller/lib/menu_screen/menu.dart
@@ -6,49 +6,67 @@ class MenuScreen extends StatelessWidget {
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-        // Manual Button
-          Container(
-            child: NeumorphicButton(
-            onPressed: () {
-              // Navigate to view page (currently named "homepage.dart")
-              Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) { 
-                return HomePage(); }
-              )
-              );
-            },
-            style: NeumorphicStyle(
-                          lightSource: LightSource.bottom,
-                          shadowDarkColor: Colors.black,
-                          shadowLightColor: Colors.black,
-                          color: Color.fromARGB(255, 14, 114, 101),
+      backgroundColor: Color.fromARGB(115, 60, 60, 60),
+      body: Center(
+        child: Container(
+          color:Color.fromARGB(255, 64, 64, 64),
+          width: 300,
+          //child: IntrinsicWidth(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Rupert!',
+                  style: TextStyle(color: Colors.black,),
+                  textAlign: TextAlign.center,),
+                Container(
+                  padding:EdgeInsets.all(15),
+                  child: Divider(
+                    thickness: 2,
+                    color: Colors.black,
+                  ),
+                ),
+          // Manual Button
+                Container(
+                  child: Align(
+                    child: SizedBox(
+                      width: 200,
+                        child: ElevatedButton(
+                            // Navigate to view page (currently named "homepage.dart")
+                            onPressed: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(builder: (context) { 
+                                  return HomePage(); 
+                                }),
+                              );
+                            },
+              
+                          child: Text('Manual',),
                         ),
-            child: Text('Manual',),
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Align(
+                    child: SizedBox(
+                      width: 200,
+                      child: ElevatedButton(  
+                        // Navigate to view page (currently named "homepage.dart")
+                        onPressed: () {
+                        Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) { 
+                          return HomePage(); })
+                        );
+                        },
+                        child: Text('Automatic',),
+                      ),
+                    ),
+                  ),
+                ),
+          ],)
         ),
-        ),
-        Container(
-            child: NeumorphicButton(
-            onPressed: () {
-              // Navigate to view page (currently named "homepage.dart")
-              Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) { 
-                return HomePage(); }
-              )
-              );
-            },
-            style: NeumorphicStyle(
-                          lightSource: LightSource.bottom,
-                          shadowDarkColor: Colors.black,
-                          shadowLightColor: Colors.black,
-                          color: Color.fromARGB(255, 14, 114, 101),
-                        ),
-            child: Text('Automatic',),
-        ),
-        )
-      ],)
-
+      //),
+      ),
     );
   }
 }

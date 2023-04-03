9ee9b736af57f276a05e678f353026c761c2e8a6
qiugai <garrettmjolly@gmail.com>
2023-04-03 10:37:31 -0400

    specified correct IP

--- ObjectRecognition/obj_rec.py
+++ ObjectRecognition/obj_rec.py
@@ -1,48 +1,50 @@
 import cv2
-import matplotlib.pyplot as plt
-import cvlib as cv
 import urllib.request
 import numpy as np
-from cvlib.object_detection import draw_bbox
-import concurrent.futures
-
-url='http://192.168.10.162/cam-hi.jpg' # replace with real IP
-im=None
-
-def run1():
-    cv2.namedWindow("live transmission", cv2.WINDOW_AUTOSIZE)
-    while True:
-        img_resp=urllib.request.urlopen(url)
-        imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
-        im = cv2.imdecode(imgnp,-1)
  
-        cv2.imshow('live transmission',im)
-        key=cv2.waitKey(5)
-        if key==ord('q'):
-            break
-            
-    cv2.destroyAllWindows()
-        
-def run2():
-    cv2.namedWindow("detection", cv2.WINDOW_AUTOSIZE)
-    while True:
-        img_resp=urllib.request.urlopen(url)
-        imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
-        im = cv2.imdecode(imgnp,-1)
+def nothing(x):
+    pass
  
-        bbox, label, conf = cv.detect_common_objects(im)
-        im = draw_bbox(im, bbox, label, conf)
+#change the IP address below according to the
+#IP shown in the Serial monitor of Arduino code
+url='http://192.168.4.1/cam-lo.jpg'
  
-        cv2.imshow('detection',im)
-        key=cv2.waitKey(5)
-        if key==ord('q'):
-            break
-            
-    cv2.destroyAllWindows()
+cv2.namedWindow("live transmission", cv2.WINDOW_AUTOSIZE)
  
-if __name__ == '__main__':
-    print("started")
-    with concurrent.futures.ProcessPoolExecutor() as executer:
-            f1= executer.submit(run1)
-            f2= executer.submit(run2)
-
+cv2.namedWindow("Tracking")
+cv2.createTrackbar("LH", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LS", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LV", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("UH", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
+ 
+while True:
+    img_resp=urllib.request.urlopen(url)
+    imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
+    frame=cv2.imdecode(imgnp,-1)
+    
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+ 
+    l_h = cv2.getTrackbarPos("LH", "Tracking")
+    l_s = cv2.getTrackbarPos("LS", "Tracking")
+    l_v = cv2.getTrackbarPos("LV", "Tracking")
+ 
+    u_h = cv2.getTrackbarPos("UH", "Tracking")
+    u_s = cv2.getTrackbarPos("US", "Tracking")
+    u_v = cv2.getTrackbarPos("UV", "Tracking")
+ 
+    l_b = np.array([l_h, l_s, l_v])
+    u_b = np.array([u_h, u_s, u_v])
+    
+    mask = cv2.inRange(hsv, l_b, u_b) 
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+ 
+    cv2.imshow("live transmission", frame)
+    cv2.imshow("mask", mask)
+    cv2.imshow("res", res)
+    key=cv2.waitKey(5)
+    if key==ord('q'):
+        break
+    
+cv2.destroyAllWindows()

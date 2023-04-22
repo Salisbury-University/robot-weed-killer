4e62c77627b650a92ebb65fc84582224860d6a5d
Zach <50252234+ZM009@users.noreply.github.com>
2023-04-22 12:42:39 -0400

    moved some of the state initialization to a user-defined mixin

--- bt_controller/lib/mixin.dart
+++ bt_controller/lib/mixin.dart
@@ -1,3 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
-mixin BluetoothHandler<T extends StatefulWidget> on State<T> {}
+mixin BluetoothHandler<T extends StatefulWidget> on State<T> {
+  // Initialize bluetooth connection state to be unknown
+  BluetoothState _bluetoothState = BluetoothState.UNKNOWN;
+  // Get instance of bluetooth
+  FlutterBluetoothSerial _bluetooth = FlutterBluetoothSerial.instance;
+  // Track bt connection
+  BluetoothConnection? connection;
+
+  late int _deviceState;
+
+  bool isDisconnecting = false;
+  var _isSwitchOn = false;
+
+  bool get isConnected => connection != null && connection!.isConnected;
+
+  // Defined to show bluetooth device list
+  List<BluetoothDevice> _devicesList = [];
+  BluetoothDevice? _device;
+  bool _connected = false;
+  bool _isButtonUnavailable = false;
+  bool _isScreenOn = false;
+
+  Map<String, Color> colors = {
+    'onBorderColor': Colors.green,
+    'offBorderColor': Colors.red,
+    'neutralBorderColor': Colors.transparent,
+    'onTextColor': Colors.green[700]!,
+    'offTextColor': Colors.red[700]!,
+    'neutralTextColor': Colors.blue,
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    FlutterBluetoothSerial.instance.state.then((state) {
+      setState(() {
+        _bluetoothState = state;
+      });
+    });
+
+    _deviceState = 0; // neutral
+
+    enableBluetooth();
+
+    // Listen for any state changes
+    FlutterBluetoothSerial.instance
+        .onStateChanged()
+        .listen((BluetoothState state) {
+      setState(() {
+        _bluetoothState = state;
+        if (_bluetoothState == BluetoothState.STATE_OFF) {
+          _isButtonUnavailable = true;
+        }
+        getPairedDevices();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    // Avoid memory leaks and disconnect
+    if (isConnected) {
+      isDisconnecting = true;
+      connection!.dispose();
+      connection = null;
+    }
+
+    super.dispose();
+  }
+
+  // Request Bluetooth permission from the user
+  Future<bool?> enableBluetooth() async {
+    // Retrieve current bluetooth state
+    _bluetoothState = await FlutterBluetoothSerial.instance.state;
+
+    // If bluetooth is off, turn it on first and retrieve paired devices
+    if (_bluetoothState == BluetoothState.STATE_OFF) {
+      await FlutterBluetoothSerial.instance.requestEnable();
+      await getPairedDevices();
+      return true;
+    } else {
+      await getPairedDevices();
+    }
+    return false;
+  }
+
+  // Retrieving and storing paired devices
+  Future<void> getPairedDevices() async {
+    List<BluetoothDevice> devices = [];
+
+    // to get list of paired devices; will catch a platform exception
+    try {
+      devices = await _bluetooth.getBondedDevices();
+    } on PlatformException {
+      debugPrint("Error");
+    }
+
+    // Unless the device state is "mounted" i.e: associated with the widget tree and attached to a BuildContext, setState will throw an error.
+    // To prevent: return from getPairedDevices()
+    if (!mounted) return;
+
+    // Store the devices list in [_devicesList] for accessing the list outside of this method
+    setState(() {
+      _devicesList = devices;
+    });
+  }
+}

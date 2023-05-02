4bb97061f7241614f058cdc67fc5a446c7cd41eb
Zach <50252234+ZM009@users.noreply.github.com>
2023-05-02 14:34:27 -0400

    removed unnecessary instances

--- bt_controller/lib/menu_screen/menu.dart
+++ bt_controller/lib/menu_screen/menu.dart
@@ -8,52 +8,13 @@ import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:page_transition/page_transition.dart';
-import 'package:webviewx/webviewx.dart';
-
-
 
 class MenuPage extends StatefulWidget {
   @override
   State<MenuPage> createState() => _MenuScreenState();
-  // webviewx
-  late WebViewXController webviewController;
 }
 
-double? deg;
-double? dist;
-
-class _MenuScreenState extends State<MenuPage> with BluetoothHandler {
-  // Initialize Bluetooth connection state to be unknown
-  BluetoothState _bluetoothState = BluetoothState.UNKNOWN;
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-  // Get instance of the Bluetooth
-  FlutterBluetoothSerial _bluetooth = FlutterBluetoothSerial.instance;
-  // Track the Bluetooth connection with the remote device
-  BluetoothConnection? connection;
-
-  late int _deviceState;
-
-  bool isDisconnecting = false;
-  var _isSwitchOn = false;
-
-  bool get isConnected => connection != null && connection!.isConnected;
-
-  // Defined for later, as needed
-  List<BluetoothDevice> _devicesList = [];
-  BluetoothDevice? _device;
-  bool _connected = false;
-  bool _isButtonUnavailable = false;
-  bool _isScreenOn = false;
-
-  Map<String, Color> colors = {
-    'onBorderColor': Colors.green,
-    'offBorderColor': Colors.red,
-    'neutralBorderColor': Colors.transparent,
-    'onTextColor': Colors.green[700]!,
-    'offTextColor': Colors.red[700]!,
-    'neutralTextColor': Colors.blue,
-  };
-
+class _MenuScreenState extends State<MenuPage> with BluetoothHandlerMixin {
   @override
   void initState() {
     super.initState();
@@ -64,31 +25,6 @@ class _MenuScreenState extends State<MenuPage> with BluetoothHandler {
     // hide sytem bar
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
         overlays: [SystemUiOverlay.bottom]);
-    // Get current state
-    FlutterBluetoothSerial.instance.state.then((state) {
-      setState(() {
-        _bluetoothState = state;
-      });
-    });
-
-    _deviceState = 0; // neutral
-
-    // If the bluetooth of the device is not enabled, then request
-    // permission to turn on bluetooth as the app starts up
-    enableBluetooth();
-
-    // Listen for further state changes
-    FlutterBluetoothSerial.instance
-        .onStateChanged()
-        .listen((BluetoothState state) {
-      setState(() {
-        _bluetoothState = state;
-        if (_bluetoothState == BluetoothState.STATE_OFF) {
-          _isButtonUnavailable = true;
-        }
-        getPairedDevices();
-      });
-    });
   }
 /*
   @override
@@ -243,7 +179,7 @@ class _MenuScreenState extends State<MenuPage> with BluetoothHandler {
                     //         type: PageTransitionType.fade, child: AutoPage()),
                     //   );
                     // },
-                      onPressed: () {
+                    onPressed: () {
                       Navigator.of(context).push(
                         MaterialPageRoute(builder: (context) {
                           return AutoPage();
@@ -301,8 +237,8 @@ class _MenuScreenState extends State<MenuPage> with BluetoothHandler {
                                   mainAxisSize: MainAxisSize.max,
                                   children: <Widget>[
                                     Visibility(
-                                      visible: _isButtonUnavailable &&
-                                          _bluetoothState ==
+                                      visible: isButtonUnavailable &&
+                                          bluetoothState ==
                                               BluetoothState.STATE_ON,
                                       child: LinearProgressIndicator(
                                         backgroundColor: Colors.yellow,
@@ -327,7 +263,7 @@ class _MenuScreenState extends State<MenuPage> with BluetoothHandler {
                                             ),
                                           ),
                                           Switch(
-                                            value: _bluetoothState.isEnabled,
+                                            value: bluetoothState.isEnabled,
                                             onChanged: (bool value) {
                                               future() async {
                                                 if (value) {
@@ -341,9 +277,9 @@ class _MenuScreenState extends State<MenuPage> with BluetoothHandler {
                                                 }
 
                                                 await getPairedDevices();
-                                                _isButtonUnavailable = false;
+                                                isButtonUnavailable = false;
 
-                                                if (_connected) {
+                                                if (connected) {
                                                   disconnect();
                                                 }
                                               }
@@ -390,20 +326,20 @@ class _MenuScreenState extends State<MenuPage> with BluetoothHandler {
                                                     items: getDeviceItems(),
                                                     onChanged: (value) =>
                                                         setState(() =>
-                                                            _device = value),
+                                                            device = value),
                                                     value:
-                                                        _devicesList.isNotEmpty
-                                                            ? _device
+                                                        devicesList.isNotEmpty
+                                                            ? device
                                                             : null,
                                                   ),
                                                   ElevatedButton(
                                                     onPressed:
-                                                        _isButtonUnavailable
+                                                        isButtonUnavailable
                                                             ? null
-                                                            : _connected
+                                                            : connected
                                                                 ? disconnect
                                                                 : connect,
-                                                    child: Text(_connected
+                                                    child: Text(connected
                                                         ? 'Disconnect'
                                                         : 'Connect'),
                                                   ),
@@ -415,11 +351,11 @@ class _MenuScreenState extends State<MenuPage> with BluetoothHandler {
                                                   const EdgeInsets.all(10.0),
                                               child: Card(
                                                 shape: RoundedRectangleBorder(
-                                                  side: new BorderSide(
-                                                      color: _deviceState == 0
+                                                  side: BorderSide(
+                                                      color: deviceState == 0
                                                           ? colors[
                                                               'neutralBorderColor']!
-                                                          : _deviceState == 1
+                                                          : deviceState == 1
                                                               ? colors[
                                                                   'onBorderColor']!
                                                               : colors[
@@ -430,7 +366,7 @@ class _MenuScreenState extends State<MenuPage> with BluetoothHandler {
                                                           4.0),
                                                 ),
                                                 elevation:
-                                                    _deviceState == 0 ? 4 : 0,
+                                                    deviceState == 0 ? 4 : 0,
                                                 child: Padding(
                                                   padding:
                                                       const EdgeInsets.all(8.0),
@@ -441,11 +377,11 @@ class _MenuScreenState extends State<MenuPage> with BluetoothHandler {
                                                           "DEVICE 1",
                                                           style: TextStyle(
                                                             fontSize: 20,
-                                                            color: _deviceState ==
+                                                            color: deviceState ==
                                                                     0
                                                                 ? colors[
                                                                     'neutralTextColor']
-                                                                : _deviceState ==
+                                                                : deviceState ==
                                                                         1
                                                                     ? colors[
                                                                         'onTextColor']

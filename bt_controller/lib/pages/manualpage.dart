d08c8e4575d1325000faa6ad70f3f750393d2939
Zach <50252234+ZM009@users.noreply.github.com>
2023-05-02 13:44:28 -0400

    removed redundnat bluetooth instance

--- bt_controller/lib/pages/manualpage.dart
+++ bt_controller/lib/pages/manualpage.dart
@@ -3,6 +3,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:bt_controller/mixin.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
@@ -18,6 +19,8 @@ import 'package:bt_controller/menu_screen/menu.dart';
 import 'package:page_transition/page_transition.dart';
 
 class HomePage extends StatefulWidget {
+  HomePage({super.key});
+
   @override
   State<HomePage> createState() => _HomePageState();
   // webviewx
@@ -27,37 +30,8 @@ class HomePage extends StatefulWidget {
 double? deg;
 double? dist;
 
-class _HomePageState extends State<HomePage> {
-  // Initialize Bluetooth connection state to be unknown
-  BluetoothState _bluetoothState = BluetoothState.UNKNOWN;
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-  // Get instance of the Bluetooth
-  FlutterBluetoothSerial _bluetooth = FlutterBluetoothSerial.instance;
-  // Track the Bluetooth connection with the remote device
-  BluetoothConnection? connection;
-
-  late int _deviceState;
-
-  bool isDisconnecting = false;
-  var _isSwitchOn = false;
-
-  bool get isConnected => connection != null && connection!.isConnected;
-
-  // Defined for later, as needed
-  List<BluetoothDevice> _devicesList = [];
-  BluetoothDevice? _device;
-  bool _connected = false;
-  bool _isButtonUnavailable = false;
-  bool _isScreenOn = false;
-
-  Map<String, Color> colors = {
-    'onBorderColor': Colors.green,
-    'offBorderColor': Colors.red,
-    'neutralBorderColor': Colors.transparent,
-    'onTextColor': Colors.green[700]!,
-    'offTextColor': Colors.red[700]!,
-    'neutralTextColor': Colors.blue,
-  };
+class _HomePageState extends State<HomePage> with BluetoothHandlerMixin {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   void initState() {
@@ -71,80 +45,8 @@ class _HomePageState extends State<HomePage> {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
         overlays: [SystemUiOverlay.bottom]);
     // Get current state
-    FlutterBluetoothSerial.instance.state.then((state) {
-      setState(() {
-        _bluetoothState = state;
-      });
-    });
-
-    _deviceState = 0; // neutral
-
-    // If the bluetooth of the device is not enabled, then request
-    // permission to turn on bluetooth as the app starts up
-    enableBluetooth();
 
     // Listen for further state changes
-    FlutterBluetoothSerial.instance
-        .onStateChanged()
-        .listen((BluetoothState state) {
-      setState(() {
-        _bluetoothState = state;
-        if (_bluetoothState == BluetoothState.STATE_OFF) {
-          _isButtonUnavailable = true;
-        }
-        getPairedDevices();
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    // Avoid any memory leaks and disconnect
-    if (isConnected) {
-      isDisconnecting = true;
-      connection!.dispose();
-      connection = null;
-    }
-
-    super.dispose();
-  }
-
-  // Request Bluetooth permission from the user
-  Future<bool?> enableBluetooth() async {
-    // Retrieve the current Bluetooth state
-    _bluetoothState = await FlutterBluetoothSerial.instance.state;
-
-    // If the bluetooth is off, then turn it on first and then retrieve
-    // the devices that are paired.
-    if (_bluetoothState == BluetoothState.STATE_OFF) {
-      await FlutterBluetoothSerial.instance.requestEnable();
-      await getPairedDevices();
-      return true;
-    } else {
-      await getPairedDevices();
-    }
-    return false;
-  }
-
-  // For retrieving and storing paired devices in a list
-  Future<void> getPairedDevices() async {
-    List<BluetoothDevice> devices = [];
-
-    // To get the list of paired devices
-    try {
-      devices = await _bluetooth.getBondedDevices();
-    } on PlatformException {
-      print("Error");
-    }
-
-    // Unless mounted is true, calling [setState] throws an error.
-    if (!mounted) return;
-
-    // Store the [devices] list in the [_devicesList] for accessing the
-    // list outside this class
-    setState(() {
-      _devicesList = devices;
-    });
   }
 
   @override
@@ -252,27 +154,27 @@ class _HomePageState extends State<HomePage> {
                         dist = distance;
                         if ((deg! >= 322 || deg! <= 32) && deg != 0) {
                           //debugPrint("FORWARD");
-                          if (_connected) {
+                          if (connected) {
                             _sendForwardMessageToBluetooth();
                           }
                         } else if (deg! >= 230 && deg! <= 305) {
                           //debugPrint("LEFT");
-                          if (_connected) {
+                          if (connected) {
                             _sendLeftMessageToBluetooth();
                           }
                         } else if (deg! >= 60 && deg! <= 125) {
                           //debugPrint("RIGHT");
-                          if (_connected) {
+                          if (connected) {
                             _sendRightMessageToBluetooth();
                           }
                         } else if (deg! >= 155 && deg! <= 225) {
                           //debugPrint("BACKWARD");
-                          if (_connected) {
+                          if (connected) {
                             _sendBackMessageToBluetooth();
                           }
                         } else if (deg == 0) {
                           //debugPrint("STOP");
-                          if (_connected) {
+                          if (connected) {
                             _sendBrakeMessageToBluetooth();
                           }
                         }
@@ -291,7 +193,7 @@ class _HomePageState extends State<HomePage> {
                     width: 100,
                     child: NeumorphicButton(
                       onPressed: () {
-                        if (_connected) {
+                        if (connected) {
                           _sendLaserOnToBluetooth();
                         }
                       },
@@ -347,85 +249,13 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  List<DropdownMenuItem<BluetoothDevice>> _getDeviceItems() {
-    List<DropdownMenuItem<BluetoothDevice>> items = [];
-    if (_devicesList.isEmpty) {
-      items.add(DropdownMenuItem(
-        child: Text('NONE'),
-      ));
-    } else {
-      _devicesList.forEach((device) {
-        items.add(DropdownMenuItem(
-          child: Text(device.name!),
-          value: device,
-        ));
-      });
-    }
-    return items;
-  }
-
-  // Method to connect to bluetooth
-  void _connect() async {
-    setState(() {
-      _isButtonUnavailable = true;
-    });
-    if (_device == null) {
-      show('No device selected');
-    } else {
-      if (!isConnected) {
-        await BluetoothConnection.toAddress(_device?.address)
-            .then((_connection) {
-          print('Connectedto the device');
-          connection = _connection;
-          setState(() {
-            _connected = true;
-          });
-
-          connection!.input!.listen(null).onDone(() {
-            if (isDisconnecting) {
-              print('Disconnecting locally!');
-            } else {
-              print('Disconnected remotely');
-            }
-            if (this.mounted) {
-              setState(() {});
-            }
-          });
-        }).catchError((error) {
-          print('Cannot connect, exception occurred');
-          print(error);
-        });
-        show('Device connected');
-
-        setState(() => _isButtonUnavailable = false);
-      }
-    }
-  }
-
-  // method to disconnect bluetooth
-  void _disconnect() async {
-    setState(() {
-      _isButtonUnavailable = true;
-      _deviceState = 0;
-    });
-
-    await connection?.close();
-    show('Device disconnected');
-    if (!connection!.isConnected) {
-      setState(() {
-        _connected = false;
-        _isButtonUnavailable = false;
-      });
-    }
-  }
-
   // method to send message
   void _sendRightMessageToBluetooth() async {
     connection!.output.add(Uint8List.fromList(utf8.encode("r")));
     await connection!.output.allSent;
     show('Device Turned Right');
     setState(() {
-      _deviceState = 1;
+      deviceState = 1;
     });
   }
 
@@ -435,7 +265,7 @@ class _HomePageState extends State<HomePage> {
     await connection!.output.allSent;
     show('Device Turned Left');
     setState(() {
-      _deviceState = -1;
+      deviceState = -1;
     });
   }
 
@@ -445,7 +275,7 @@ class _HomePageState extends State<HomePage> {
     await connection!.output.allSent;
     show('Device Moved Forward');
     setState(() {
-      _deviceState = 1;
+      deviceState = 1;
     });
   }
 
@@ -454,7 +284,7 @@ class _HomePageState extends State<HomePage> {
     await connection!.output.allSent;
     show('Device Moved Backward');
     setState(() {
-      _deviceState = -1;
+      deviceState = -1;
     });
   }
 
@@ -463,7 +293,7 @@ class _HomePageState extends State<HomePage> {
     await connection!.output.allSent;
     show('Device Stopped');
     setState(() {
-      _deviceState = -1;
+      deviceState = -1;
     });
   }
 
@@ -471,24 +301,7 @@ class _HomePageState extends State<HomePage> {
     connection!.output.add(Uint8List.fromList(utf8.encode("+")));
     await connection!.output.allSent;
     setState(() {
-      _deviceState = -1;
+      deviceState = -1;
     });
-  }
-
-  Future show(
-    String message, {
-    Duration duration: const Duration(seconds: 3),
-  }) async {
-    await new Future.delayed(new Duration(milliseconds: 100));
-    // tryhing without below method
-    //_scaffoldKey.currentState!.showSnackBar(
-    ScaffoldMessenger.of(context).showSnackBar(
-      new SnackBar(
-        content: new Text(
-          message,
-        ),
-        duration: duration,
-      ),
-    );
   }
 }

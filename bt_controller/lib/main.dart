c59c802b877b37b332f7bcea1ab4416900472c42
Zach <50252234+ZM009@users.noreply.github.com>
2023-05-23 20:38:40 -0400

    wrapped app in change provider

--- bt_controller/lib/main.dart
+++ bt_controller/lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import './menu_screen/menu.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:bt_controller/connection.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   SystemChrome.setPreferredOrientations(
           [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
-      .then((_) => runApp(RoundupApp()));
+      .then((_) => runApp(ChangeNotifierProvider(
+            create: (_) => BluetoothHandlerProvider(),
+            child: RoundupApp(),
+          )));
 }
 
 class RoundupApp extends StatelessWidget {
-  const RoundupApp({super.key});
+  const RoundupApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
